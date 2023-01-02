450970e8b9b3315622e84b7312f9b45e8a161c30
Dmitriy Afonichkin <dafonichkin@gmail.com>
2023-01-03 00:51:16 +0300

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

---
+++ src/main/java/ru/job4j/grabber/MemStore.java
@@ -0,0 +1,37 @@
+package ru.job4j.grabber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemStore implements Store {
+    private final List<Post> posts = new ArrayList<>();
+    private int ids = 1;
+
+    @Override
+    public void save(Post post) {
+        post.setId(ids++);
+        posts.add(post);
+    }
+
+    @Override
+    public List<Post> getAll() {
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? posts.get(index) : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < posts.size(); index++) {
+            if (posts.get(index).getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
