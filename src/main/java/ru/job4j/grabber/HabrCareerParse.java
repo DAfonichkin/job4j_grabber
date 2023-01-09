1b8a192faad267e090e9a494ae1cd8611748d8f4
Dmitriy Afonichkin <dafonichkin@gmail.com>
2023-01-10 01:02:47 +0300

    2.4. HabrCareerParse [#285213 #376367]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -6,14 +6,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.DateTimeParser;
-import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HabrCareerParse implements Parse {
-
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
     private static final int PAGE_COUNT = 5;
@@ -23,60 +21,50 @@ public class HabrCareerParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
-    public static void main(String[] args) {
-        Parse parser = new HabrCareerParse(new HabrCareerDateTimeParser());
-        List<Post> posts = new ArrayList<>();
-        for (int i = 1; i <= PAGE_COUNT; i++) {
-            String link = PAGE_LINK + "?page=" + i;
-            posts.addAll(parser.list(link));
-        }
-        posts.forEach(System.out::println);
-    }
-
-    private static String retrieveDescription(String link) throws IOException {
+    private String retrieveDescription(String link) {
         StringBuilder rsl = new StringBuilder();
         Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
-        Elements rows = document.select(".style-ugc");
-        rows.forEach(row -> {
-            for (int i = 0; i < row.childNodeSize(); i++) {
-                rsl.append(Jsoup.parse(row.childNode(i).toString()).text());
-                rsl.append(System.lineSeparator());
-            }
-        });
+        try {
+            Document document = connection.get();
+            Elements rows = document.select(".style-ugc");
+            rows.forEach(row -> {
+                for (int i = 0; i < row.childNodeSize(); i++) {
+                    rsl.append(Jsoup.parse(row.childNode(i).toString()).text());
+                    rsl.append(System.lineSeparator());
+                }
+            });
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
         return rsl.toString();
     }
 
-    private List<Post> getPostsByElement(Elements rows) {
-        List<Post> postList = new ArrayList<>();
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            Element dateElement = row.select(".vacancy-card__date").first().child(0);
-            String date = dateElement.attr("datetime");
-            String vacancyName = titleElement.text();
-            String vacancyLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            String description;
-            try {
-                description = retrieveDescription(vacancyLink);
-            } catch (IOException e) {
-                throw new IllegalArgumentException();
-            }
-            postList.add(new Post(vacancyLink, vacancyName, description, dateTimeParser.parse(date)));
-        });
-        return postList;
+    private Post getPost(Element row) {
+        Element titleElement = row.select(".vacancy-card__title").first();
+        Element linkElement = titleElement.child(0);
+        Element dateElement = row.select(".vacancy-card__date").first().child(0);
+        String date = dateElement.attr("datetime");
+        String vacancyName = titleElement.text();
+        String vacancyLink = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+        String description;
+        description = retrieveDescription(vacancyLink);
+        return new Post(vacancyLink, vacancyName, description, dateTimeParser.parse(date));
     }
 
     @Override
     public List<Post> list(String link) {
-        Connection connection = Jsoup.connect(link);
-        Document document;
-        try {
-            document = connection.get();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        List<Post> posts = new ArrayList<>();
+        for (int i = 1; i <= PAGE_COUNT; i++) {
+            Connection connection = Jsoup.connect(link + "?page=" + i);
+            Document document;
+            try {
+                document = connection.get();
+            } catch (IOException e) {
+                throw new IllegalArgumentException();
+            }
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> posts.add(getPost(row)));
         }
-        Elements rows = document.select(".vacancy-card__inner");
-        return getPostsByElement(rows);
+        return posts;
     }
 }
